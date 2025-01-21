476986e7cf3b76f16d26682095282a4f4b78c8aa
Yuval Eyal <uveyal@gmail.com>
2025-01-21 14:29:22 +0200

    relation structure class done!!

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ if __name__ == "__main__":
     nlp = spacy.load("en_core_web_sm")
     page = wikipedia.page('Brad Pitt').content
     analyzed_page = nlp(page)
+
+    # Get POS tags
+    pos_tags = {}
+    for token in analyzed_page:
+        pos_tags[token.text] = token.pos_
+    
+    
