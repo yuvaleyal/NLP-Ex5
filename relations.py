e9b46281ca824a8337fa060f929a4a355d303fb0
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-01-22 14:22:44 +0200

    added token relations

--- relations.py
+++ relations.py
@@ -1,1 +1,5 @@
 COMPOUND_RELATION = "compound"
+NSUBJ_RELATION = "nsubj" # nominal subject
+DOBJ_RELATION = "dobj" # direct object
+PREP_RELATION = "prep" # preposition
+POBJ_RELATION = "pobj" # prepositional object
