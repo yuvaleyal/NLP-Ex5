170e1aa01b19220e90c0bb7e5bde6c56711b6a57
Yuval Eyal <uveyal@gmail.com>
2025-01-29 16:00:06 +0200

    LMM part

--- LLM_facade.py
+++ LLM_facade.py
@@ -1,7 +1,27 @@
 import google.generativeai as genai
+import spacy
+import wikipedia
+from spacy.tokens.doc import Doc
+from relation_structure import RelationStructure
 KEY = "AIzaSyC92XWZEYYG0bAsp5KlfDZrMGdAaKPl9QE"
-if __name__ == "__main__":
+PROMPT_PERFIX = "read the following text end extracts triplets of (Subject, Relation,Object), where each of them is a span of text. The Subject and Object slot fillers are names (proper nouns), and the Relation slot filler is a verb or a verb along with a preposition. Return only the relations you find, each in a seperate line and formatted as follows: [Subject, Relation, Object]. The text is: "
+
+def LLM_respeonse(text: Doc):
+    prompt = PROMPT_PERFIX + text.text
     genai.configure(api_key=KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Explain how AI works")
-    print(response.text)
+    response = model.generate_content(prompt)
+    return response.text.split("\n")
+
+def triplet_to_relation(triplet: str) -> RelationStructure:
+    triplet = triplet[1:-1]
+    subject, relation, obj = triplet.split(", ")
+    return RelationStructure(subject, relation, obj)
+
+def LLM_relations(text: Doc) -> list[RelationStructure]:
+    triplets = LLM_respeonse(text)
+    relations = []
+    for triplet in triplets:
+        if triplet:
+            relations.append(triplet_to_relation(triplet))
+    return relations
